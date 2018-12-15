0ba3193a3c0073cccbad641f08e996457f8c38f7
Mike iLL Kilmer <mike@mzoo.org>
2018-12-14 23:29:07 -0600

    Correct code on uninstall file.

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,12 @@
 <?php
-/*
-Plugin Name: Display Full Image Dimensions
-Description: Display and sort by dimensions of image in Media Library listing.
-Author: Mike iLL/mZoo
-Version: 1.0.0
-Author URI: http://mzoo.org
-Text Domain: display-full-image-dimensions
-*/
+/**
+ * Runs on Uninstall of Display Full Image Dimensions
+ *
+ * @package   Display Full Image Dimensions
+ * @author    Mike iLL/mZoo
+ * @license   GPL-2.0+
+ * @link      http://mzoo.org
+ */
 
 // Check that we should be doing this
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
